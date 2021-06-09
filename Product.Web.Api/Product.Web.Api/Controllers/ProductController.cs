ec33fc44c6c37711b706574ccd42d848b5d0c894
LeandroReis <40226937+LeandroReis1997@users.noreply.github.com>
2021-06-09 17:32:19 -0300

    Criando API produtos com MongoDB

---
+++ Product.Web.Api/Product.Web.Api/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Product.Web.Api.Models;
+using Product.Web.Api.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Product.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProductModel>> Get()
+        {
+            return _productService.Get();
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        public ActionResult<ProductModel> GetById(string id)
+        {
+            var product = _productService.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<ProductModel> Create(ProductModel product)
+        {
+            _productService.Create(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id.ToString() }, product);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, ProductModel productModel)
+        {
+            var product = _productService.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productService.Update(id, productModel);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult DeleteById(string id)
+        {
+            var product = _productService.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productService.Remove(product.Id);
+
+            return NoContent();
+        }
+
+    }
+}
