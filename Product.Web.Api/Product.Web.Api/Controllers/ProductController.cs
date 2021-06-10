b27bc82754eb8e60a836565cb47de7516dd49261
LeandroReis <40226937+LeandroReis1997@users.noreply.github.com>
2021-06-10 02:17:06 -0300

    Status Code

--- Product.Web.Api/Product.Web.Api/Controllers/ProductController.cs
+++ Product.Web.Api/Product.Web.Api/Controllers/ProductController.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Product.Web.Api.DTO.Product;
 using Product.Web.Bll.Interface;
 using Product.Web.Info;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Product.Web.Api.Controllers
@@ -22,12 +25,23 @@ namespace Product.Web.Api.Controllers
         }
 
         [HttpGet]
+        [Produces(typeof(IEnumerable<ProductListDTO>))]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "OK", Type = typeof(IEnumerable<ProductListDTO>))]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "OK", Type = typeof(ProductListDTO))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, Description = "Erro de Autenticação")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound, Description = "Recurso não encontrado")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Erro na API")]
         public IActionResult Get()
         {
             return Ok(_mapper.Map<IList<ProductListDTO>>(bll.GetAllProducts()));
         }
 
         [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [Produces(typeof(IEnumerable<ProductListDTO>))]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "OK", Type = typeof(ProductListDTO))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, Description = "Erro de Autenticação")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound, Description = "Recurso não encontrado")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Erro na API")]
         public IActionResult GetById(string id)
         {
             var product = bll.GetByProductId(id);
@@ -41,6 +55,11 @@ namespace Product.Web.Api.Controllers
         }
 
         [HttpGet("{name}")]
+        [Produces(typeof(IEnumerable<ProductListDTO>))]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "OK", Type = typeof(ProductListDTO))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, Description = "Erro de Autenticação")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound, Description = "Recurso não encontrado")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Erro na API")]
         public IActionResult GetByName(string name)
         {
             var product = bll.GetByName(name);
@@ -50,10 +69,16 @@ namespace Product.Web.Api.Controllers
                 return NotFound();
             }
 
-            return Ok(bll.GetByName(name));
+            return Ok(_mapper.Map<List<ProductListDTO>>(bll.GetByName(name)));
         }
 
         [HttpPost]
+        [Produces(typeof(ProductDTO))]
+        [SwaggerResponse((int)HttpStatusCode.Created, Description = "Inserido com sucesso", Type = typeof(ProductDTO))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Requisição mal-formatada")]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, Description = "Erro de Autenticação")]
+        [SwaggerResponse((int)HttpStatusCode.Conflict, Description = "Conflito")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Erro na API")]
         public async Task<IActionResult> Create(ProductDTO product)
         {
             var productDTO = await bll.AddProductAsync(_mapper.Map<ProductInfo>(product));
@@ -62,21 +87,33 @@ namespace Product.Web.Api.Controllers
         }
 
         [HttpPut("{id:length(24)}")]
+        [Produces(typeof(ProductDTO))]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Alterado com sucesso", Type = typeof(ProductDTO))]
+        [SwaggerResponse((int)HttpStatusCode.BadRequest, Description = "Requisição mal-formatada")]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, Description = "Erro de Autenticação")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound, Description = "Recurso não encontrado")]
+        [SwaggerResponse((int)HttpStatusCode.Conflict, Description = "Conflito")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Erro na API")]
         public async Task<IActionResult> Update(string id, ProductDTO productDTO)
         {
             var product = bll.GetByProductId(id);
-            
+
             if (product == null)
             {
                 return NotFound();
             }
 
-            await bll.EditProductAsync(id, _mapper.Map<ProductInfo>(productDTO));
+            var productObject = await bll.EditProductAsync(id, _mapper.Map<ProductInfo>(productDTO));
 
-            return new ObjectResult(_mapper.Map<ProductInfo>(product));
+            return new ObjectResult(_mapper.Map<ProductInfo>(productObject));
         }
 
         [HttpDelete("{id:length(24)}")]
+        [Produces(typeof(OkResult))]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Removido com sucesso")]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, Description = "Erro de Autenticação")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound, Description = "Recurso não encontrado")]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError, Description = "Erro na API")]
         public IActionResult DeleteById(string id)
         {
             var product = bll.GetByProductId(id);
